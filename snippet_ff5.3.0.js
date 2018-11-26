0025742e818c3173aaa679e5cb9714df61845b79
marcbadinier <36453068+marcbadinier@users.noreply.github.com>
2018-11-26 12:06:22 +0100

    Short version of snippet's code
    Another way to code : Enum with Array.from method instead of the precedent classic loop.

---
+++ snippet_ff5.3.0.js
@@ -0,0 +1,11 @@
+var listProdView = []; var found = false;
+//  Fill function () : for each item in array (selected by class 'product-container'), if the item is in viewport -> if the item is not found into the viewed list's array : add item to array & console.
+function fillConsole(){ Array.from(document.getElementsByClassName("product-container")).forEach(function(item) {
+    if( item.getBoundingClientRect().bottom > 0 && window.innerHeight - item.getBoundingClientRect().bottom > 0){//todo ici
+        for(let i = 0; i < listProdView.length; i++) { if (listProdView[i] == (item.outerText.replace(/[\n\r]+/g, ' ')).replace('EXCLU WEB ', '')) {found = true;} }
+        if (!found){ console.log((item.outerText.replace(/[\n\r]+/g, ' ')).replace('EXCLU WEB ', '')); listProdView.push((item.outerText.replace(/[\n\r]+/g, ' ')).replace('EXCLU WEB ', '')); }
+        found = false; } } ); }
+//  first load
+fillConsole();
+//   Event : scrolling
+document.addEventListener('scroll', function(e) { fillConsole() });
